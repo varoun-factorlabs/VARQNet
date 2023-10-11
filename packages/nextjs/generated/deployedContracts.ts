923f4fc8a41fe3e924208eabbfe38b468381139e
Darius Ramnath <39842396+Dariusramnath@users.noreply.github.com>
2023-10-11 00:04:53 -0400

    Vttdc balance added, reset of input fields, minor name changes to Heading

--- packages/nextjs/generated/deployedContracts.ts
+++ packages/nextjs/generated/deployedContracts.ts
@@ -5,7 +5,7 @@ const contracts = {
       name: "localhost",
       contracts: {
         Backed_bTTDC: {
-          address: "0x0165878A594ca255338adfa4d48449f69242Eb8F",
+          address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
           abi: [
             {
               inputs: [
@@ -291,7 +291,7 @@ const contracts = {
           ],
         },
         Mock_USDC: {
-          address: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
+          address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
           abi: [
             {
               inputs: [
@@ -919,7 +919,7 @@ const contracts = {
           ],
         },
         Vault: {
-          address: "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853",
+          address: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
           abi: [
             {
               inputs: [
