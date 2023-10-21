f54bd47047e0d75b288fce670a0c83dbf5db0294
Darius Ramnath <39842396+Dariusramnath@users.noreply.github.com>
2023-10-21 11:26:59 -0400

    Changes to deploy script

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -95,36 +95,6 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
     autoMine: true,
   });
 
-  const Fees = await deploy("Fees", {
-    from: deployer,
-    // Contract constructor arguments
-    args: [], //[deployer],
-    log: true,
-    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
-    // automatically mining the contract deployment transaction. There is no effect on live networks.
-    autoMine: true,
-  });
-
-  const Owned = await deploy("Owned", {
-    from: deployer,
-    // Contract constructor arguments
-    args: [], //[deployer],
-    log: true,
-    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
-    // automatically mining the contract deployment transaction. There is no effect on live networks.
-    autoMine: true,
-  });
-
-  const PoolManager = await deploy("PoolManager", {
-    from: deployer,
-    // Contract constructor arguments
-    args: [], //[deployer],
-    log: true,
-    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
-    // automatically mining the contract deployment transaction. There is no effect on live networks.
-    autoMine: true,
-  });
-
   // address _owner,
   // address _usdc,
   // address _bTTDC,
